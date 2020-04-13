297b761bbb6e016caaa2f1f35e4961ca2c6509f9
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-04-13 11:14:53 +0430

    add memo page, add brand bg gradient

---
+++ src/views/MemoAdd.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <PageNavigation />
+    <div class="container mx-auto bg-brand-gradient">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum accusamus placeat nesciunt repellendus veritatis laborum, perferendis laudantium distinctio eos. Placeat tenetur laudantium, fugit explicabo corporis eos est quibusdam soluta reiciendis minus, omnis excepturi hic nostrum quidem sequi quasi accusamus facere dignissimos saepe? Dolor nihil similique porro aliquid perspiciatis neque debitis distinctio saepe perferendis veniam, incidunt, ut qui ratione optio dicta non ab pariatur dignissimos delectus. Est veniam excepturi magni laborum beatae explicabo quisquam et, non modi nemo voluptates saepe magnam qui distinctio delectus eum quis exercitationem accusamus nam. Reprehenderit, ratione ipsa quaerat similique totam dicta sed! Temporibus, ab aperiam? Obcaecati.
+    </div>
+  </div>
+</template>
+
+<script>
+import PageNavigation from '@/components/PageNavigation.vue';
+// import InputText from '@/components/BaseInputText.vue';
+
+export default {
+  name: 'MemoAdd',
+  components: { PageNavigation }
+};
+</script>
